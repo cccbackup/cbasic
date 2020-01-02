1b36e9042d57e7ceb62cbf0d7c333da940948234
ccckmit <ccckmit@gmail.com>
2020-01-02 09:03:55 +0800

    add callptr2

--- callptr2.c
+++ callptr2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int f1(int n) {
-  n=3;
-  printf("n=%d\n", n);
-  return n;
+int f1(int *p) {
+  *p=3;
+  printf("*p=%d\n", *p);
+  return *p;
 }
 
 int main() {
   int a = 5;
   printf("a=%d\n", a);
-  int r = f1(a);
+  int r = f1(&a);
   printf("r=%d\n", r);
   printf("a=%d\n", a);
 }
